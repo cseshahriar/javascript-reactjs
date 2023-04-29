34101af7ccde0c701b6654baac05f888c8521876
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-04-29 19:24:47 +0600

    input change

--- basics/src/App.js
+++ basics/src/App.js
@@ -8,7 +8,8 @@ class App extends Component{
             { id: 1, bookName: 'book1', writer: 'Shahriar'},
             {id: 2, bookName: 'book3', writer: 'Shahriar Hosen'},
             {id: 3, bookName: 'book3', writer: 'Shahriar Hosen'}
-        ]
+        ],
+        text: '',
     }
     changeBookState = () => {
         this.setState({
@@ -20,12 +21,22 @@ class App extends Component{
         })
     }
 
+    changeInputWithState = (event) => {
+        this.setState({
+            text: event.target.value
+        })
+    }
+
     render() {
         return (
             <div className="App">
                 <header className="App-header">
                     <h3>State change with event and pass a method with props</h3>
                     <Book data={this.state.books} change={this.changeBookState} />
+
+                    <h1>Input Change</h1>
+                    <p>{ this.state.text }</p>
+                    <input type='text' name='text' id='text' onChange={this.changeInputWithState} />
                 </header>
             </div>
         );
