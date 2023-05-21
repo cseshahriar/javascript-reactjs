e58e1dc7a53f46141b2b4a0f0588824a4093eb5d
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-05-21 14:31:39 +0600

    restaurant project

---
+++ restaurant/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Menu from './Menu';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+               <Menu />
+            </div>
+        );
+    }
+}
+
+export default Home;
---
+++ restaurant/src/components/MainComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Body from './Body';
+
+
+const MainComponent = () => {
+    return (
+        <div>
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+    );
+}
+
+export default MainComponent;
